fca4ce34b6ac43703e78d32ef93537ad08eeadd4
Desislava Hadzhiolova <desislava1.hadzhiolova@st.oth-regensburg.degit config --global user.email desislava1.hadzhiolova@st.oth-regensburg.de>
2022-01-18 18:04:01 +0100

    Implemented Login and Sign Up functionality for the App

--- app/src/main/java/ia/wit/groups_desislavahad/Login.kt
+++ app/src/main/java/ia/wit/groups_desislavahad/Login.kt
@@ -3,7 +3,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import timber.log.Timber
 
 
@@ -13,6 +15,7 @@ class Login : AppCompatActivity() {
     private lateinit var addPassword : EditText
     private lateinit var buttonLogin: Button
     private lateinit var buttonSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,7 @@ class Login : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
         Timber.i("Login Activity started.")
 
+        mAuth= FirebaseAuth.getInstance()
 
         addEmail = findViewById(R.id.add_email)
         addPassword = findViewById(R.id.add_password)
@@ -31,5 +35,28 @@ class Login : AppCompatActivity() {
             startActivity(intent)
         }
 
+        buttonLogin.setOnClickListener{
+            val email = addEmail.text.toString()
+            val password = addPassword.text.toString()
+
+            login(email,password)
+        }
+
+
     }
+
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this@Login, MainActivity:: class.java)
+                    startActivity(intent)
+
+                } else {
+                   Toast.makeText(this@Login, "User does not exist", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 }
--- app/src/main/java/ia/wit/groups_desislavahad/SignUp.kt
+++ app/src/main/java/ia/wit/groups_desislavahad/SignUp.kt
@@ -1,11 +1,56 @@
 package ia.wit.groups_desislavahad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.ActionCodeEmailInfo
+import com.google.firebase.auth.FirebaseAuth
+import timber.log.Timber
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var addName: EditText
+    private lateinit var addEmail: EditText
+    private lateinit var addPassword : EditText
+    private lateinit var buttonSignUp: Button
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+        Timber.i("SignUp Activity started.")
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+        addName = findViewById(R.id.add_name)
+        addEmail = findViewById(R.id.add_email)
+        addPassword = findViewById(R.id.add_password)
+        buttonSignUp = findViewById(R.id.button_Signup)
+
+        buttonSignUp.setOnClickListener(){
+            val email = addEmail.text.toString()
+            val password = addPassword.text.toString()
+
+            signUp(email, password)
+
+        }
+
     }
+
+    private fun signUp(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //return to home
+                    val intent = Intent(this@SignUp, MainActivity:: class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@SignUp, "Error while logging in", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
